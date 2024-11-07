c5bc5046f90f68a819eb0b6c0132e6fcd212d1e0
Anthony Ngonde <anto.ngonde@gmail.com>
2024-11-07 13:03:14 +0300

    Fetched signup data from backend

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 
 const Members = () => {
+    const [signUp, setSignUp] = useState([]);
+
+    useEffect(
+        ()=>{
+            fetch('http://localhost:5000/signup')
+            .then(res=>res.json())
+            .then(data=>console.log(data))
+            .catch(err=>console.log(err))
+        },[]
+    )
+
+
   return (
     <div className='members'>
         <h1>Members Page</h1>
