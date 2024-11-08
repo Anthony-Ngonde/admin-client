65eaca3665fbdede8d6d6b70898b1a2411f9dac4
Anthony Ngonde <anto.ngonde@gmail.com>
2024-11-08 12:52:19 +0300

    Added delete functionality to the members page

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import SignUp from '../SignUp/SignUp';
+// import {Modal} from 'react-bootstrap'
 
 
 
 
 const Members = () => {
     const [signUp, setSignUp] = useState([]);
+    const [show, setShow] = useState(false)
 
     useEffect(() => {
         fetch('http://localhost:5000/signup')
@@ -15,10 +17,51 @@ const Members = () => {
                 setSignUp(data.users || []);
             })
             .catch(err => console.log(err));
-    }, []);
+    }, []
+
+    );
+
+    const deleteMember=(id)=>{
+        console.log(id)
+
+
+        const requestOptions = {
+            method:"DELETE",
+            headers:{
+                'content-type':'application/json'
+            },
+        }
+
+        fetch(`http://localhost:5000/signup/${id}`, requestOptions)
+        .then(res=>res.json())
+        .then(data=>{console.log(data)})
+        .catch(err=>console.log(err))
+    }
+
+    // const closeModal=()=>{
+    //     setShow(false)
+    // }
+
+    // const showModal=()=>{
+    //     setShow(true)
+    // }
 
     return (
         <div className='members'>
+            {/* <Modal
+                show={show}
+                size='lg'
+                onHide={closeModal}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Update Member
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Update</p>
+                </Modal.Body>
+            </Modal> */}
             <h1>List of Members</h1>
             <table className="members-table">
                 <thead>
@@ -36,6 +79,9 @@ const Members = () => {
                             MemberId={member.id}
                             Name={member.name}
                             Email={member.email}
+                            // onClick={showModal}
+
+                            onDelete={()=>{deleteMember(member.id)}}
                         />
                     ))}
                 </tbody>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+// import {Modal} from 'react-bootstrap'
 import './SignUp.css'
 
 
 
-const SignUp = ({ MemberId, Name, Email }) => {
+const SignUp = ({ MemberId, Name, Email, onDelete }) => {
     return (
         <tr>
             <td>{MemberId}</td>
             <td>{Name}</td>
             <td>{Email}</td>
             <td>
-                <button className="update-btn">Update</button>
-                <button className="delete-btn">Delete</button>
+                {/* <button className="update-btn" variant="primary" onClick={onClick}>Update</button> */}
+                <button className="delete-btn" onClick={onDelete}>Delete</button>
             </td>
         </tr>
     );
